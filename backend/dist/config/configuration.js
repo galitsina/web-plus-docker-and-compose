8ac98bee3ee62b92f5924cde5ade44b43b7f6ca3
Daniella Galitsyna <galitsina@bstec.ru>
2024-02-24 22:29:56 +0100

    dockerized FE and BE, add docker-compose

--- backend/dist/config/configuration.js
+++ backend/dist/config/configuration.js
@@ -2,11 +2,11 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.default = () => ({
     database: {
-        host: process.env.DB_HOST || 'localhost',
+        host: process.env.POSTGRES_HOST || 'localhost',
         port: process.env.DB_PORT || 5432,
         username: process.env.DB_USER || 'student',
-        password: process.env.DB_PASSWORD || 'student',
-        database: process.env.DB_NAME || 'kupipodariday',
+        password: process.env.POSTGRES_USER || 'student',
+        database: process.env.POSTGRES_DB || 'kupipodariday',
     },
     jwt: {
         secret: process.env.JWT_SECRET || 'secret',
